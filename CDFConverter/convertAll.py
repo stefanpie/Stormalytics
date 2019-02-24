9439e873e9cf67ff29dfabc2eca69f0f5399a237
ksarangmath <kartiksarangmath@gmail.com>
2019-02-24 12:13:03 -0500

    Update convertAll.py
    removed sys import

--- CDFConverter/convertAll.py
+++ CDFConverter/convertAll.py
@@ -1,9 +1,8 @@
 import subprocess
-import sys
 import glob
 path = '.'
 filenames = glob.glob(path + '/in/*')
-for filename in filenames:
-	subprocess.call(["java", "-classpath", "toolsUI-4.6.11.jar", "ucar.nc2.FileWriter", "-in", filename, "-out", filename + ".nc"])
-	subprocess.call(["rm", filename + ".uncompress"])
-	subprocess.call(["mv", filename + ".nc", "./out"])
+for filename in filenames: #runs through all files in ./in
+	subprocess.call(["java", "-classpath", "toolsUI-4.6.11.jar", "ucar.nc2.FileWriter", "-in", filename, "-out", filename + ".nc"]) #uses unidata tool to convert files
+	subprocess.call(["rm", filename + ".uncompress"]) #deletes uncompressed files
+	subprocess.call(["mv", filename + ".nc", "./out"]) #moves files to ./out
