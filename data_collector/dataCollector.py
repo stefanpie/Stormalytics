883d8dee12230d6f12c398c6d69b16df881365ff
Kartik Sarangmath <ksarangmath@res388d-128-61-91-133.res.gatech.edu>
2019-04-13 18:12:13 -0400

    updated data collector

--- data_collector/dataCollector.py
+++ data_collector/dataCollector.py
@@ -1,20 +1,32 @@
 import requests
+import os
 import urllib
 import bs4
+import glob
+import subprocess
+
 #testfile = urllib.URLopener()
-res = requests.get('https://nomads.ncep.noaa.gov/pub/data/nccf/radar/nexrad_level2/')
+res = requests.get('https://mesonet-nexrad.agron.iastate.edu/level2/raw/')
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 i = 0
 for link in soup.find_all('a'):
 	k = 0
-	if (i >= 2):
-		res1 = requests.get('https://nomads.ncep.noaa.gov/pub/data/nccf/radar/nexrad_level2/' + link['href'])
+	if (i >= 6):
+		print(link['href'])
+		res1 = requests.get('https://mesonet-nexrad.agron.iastate.edu/level2/raw/' + link['href'])
 		soup1 = bs4.BeautifulSoup(res1.text, 'lxml')
 		for link1 in soup1.find_all('a'):
-			if (k >= 2):
+			if (k >= 6):
 				if (link1.find_next("a") == None):
 					#testfile.retrieve('https://nomads.ncep.noaa.gov/pub/data/nccf/radar/nexrad_level2/' + link['href'] + link1['href'], "./out/" + link['href'])
-					print(link['href'] + link1['href'])
+					os.system("wget -O ./in/" + link['href'][:-1] + " https://mesonet-nexrad.agron.iastate.edu/level2/raw/" + link['href'] + link1['href'])
+					print(link['href'] + link1.find_previous("a")['href'])
 			k = k + 1
-		print(link['href'])
 	i = i + 1
+
+path = '.'
+filenames = glob.glob(path + '/in/*')
+for filename in filenames: #runs through all files in ./in
+	subprocess.call(["java", "-classpath", "toolsUI-4.6.11.jar", "ucar.nc2.FileWriter", "-in", filename, "-out", filename + ".nc"]) #uses unidata tool to convert files
+	subprocess.call(["rm", filename + ".uncompress"]) #deletes uncompressed files
+	subprocess.call(["mv", filename + ".nc", "./out"]) #moves files to ./out
